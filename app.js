47e5eea2a75c0cf694b5af8677530f40d4d8faa6
sudexp <sudexp@icloud.com>
2019-10-29 13:45:19 +0200

    template engine pug added to the project, views created, app.js modified with engine

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const collectionRouter = require('./routes/collection');
 
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,12 +23,10 @@ app.use('/', indexRouter);
 app.use('/collection', collectionRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use((req, res, next) => next(createError(404)));
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/collection.js
+++ routes/collection.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 /* GET collection listing. */
-router.get('/', (req, res, next) => res.send('This is the route for collection.'));
+router.get('/', (req, res, next) => res.send('This is the route for collection'));
 
 module.exports = router;
